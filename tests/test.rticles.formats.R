f669883fb4899951ca5f429d3b25d8c134ecd8fd
dmkaplan2000 <David M. Kaplan>
2020-12-15 16:53:21 +0100

    Adding small discussion of how we would make a combined format

--- tests/test.rticles.formats.R
+++ tests/test.rticles.formats.R
@@ -1,3 +1,7 @@
-xx = draft.list.templates()
+library(testicles)
+#dd = tempdir(check=TRUE)
+dd = "test.rticles.formats.dir"
+dir.create(dd)
+xx = draft.list.templates(target.dir=dd)
 file.remove(xx$rmd.filenames[1])
 yy = render.list.rmd(xx$rmd.filenames)
